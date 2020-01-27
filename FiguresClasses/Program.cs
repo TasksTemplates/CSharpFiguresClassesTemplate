32abfc91ae1460d301d3f5acf89778476c1b13a6
oleggolen <oleg-golenischev63@yandex.ru>
2020-01-27 22:41:21 +0700

    Добавьте файлы проекта.

---
+++ FiguresClasses/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FiguresClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
